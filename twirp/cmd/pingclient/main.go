8ab558a18c9620b0e716946b3aa9ab22a6e6e6fa
Marcos Hack <marcoshack@gmail.com>
2024-12-22 00:00:00 -0500

    add CLI options to twirp/cmd/pingclient

--- twirp/cmd/pingclient/main.go
+++ twirp/cmd/pingclient/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -11,23 +13,42 @@ import (
 )
 
 func main() {
+
+	var clientType, baseURL string
+	var numRequests int
+	flag.StringVar(&baseURL, "s", "http://localhost:8080", "Server endpoint")
+	flag.StringVar(&clientType, "t", "protobuf", "Type of client to use (json or protobuf)")
+	flag.IntVar(&numRequests, "n", 1, "Number of requests to make")
+	flag.Parse()
+
 	// initialize zerolog logger
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "15:04:05"}
 	logger := zerolog.New(output).With().Timestamp().Logger()
 
 	// initialize twirp client
 	httpClient := &http.Client{}
-	baseURL := "http://localhost:8080"
-	twirpClient := api.NewPingServiceProtobufClient(baseURL, httpClient)
+	var twirpClient api.PingService
+	switch clientType {
+	case "protobuf":
+		twirpClient = api.NewPingServiceProtobufClient(baseURL, httpClient)
+	case "json":
+		twirpClient = api.NewPingServiceJSONClient(baseURL, httpClient)
+	}
+	logger.Info().Str("client", clientType).Str("baseURL", baseURL).Msg("twirp client initialized")
 
 	// contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	start := time.Now()
-	response, err := twirpClient.Ping(ctx, &api.PingRequest{Message: "Hello, Twirp!"})
-	elapsed := time.Since(start)
-	if err != nil {
-		logger.Error().Err(err).Msg("could not ping")
+	avgLatency := time.Duration(0)
+	for i := 0; i < numRequests; i++ {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		start := time.Now()
+		response, err := twirpClient.Ping(ctx, &api.PingRequest{Message: "Hello, Twirp!"})
+		latency := time.Since(start)
+		avgLatency += latency
+		if err != nil {
+			logger.Error().Err(err).Msg("could not ping")
+		}
+		logger.Info().Str("response", response.GetMessage()).Str("latency", latency.String()).Msg("PING response received")
 	}
-	logger.Info().Str("response", response.GetMessage()).Str("elapsed", elapsed.String()).Msg("PING response received")
+	logger.Info().Int("numRequests", numRequests).Str("avgLatency", fmt.Sprintf("%dµs", avgLatency.Microseconds()/int64(numRequests))).Msg("done")
 }
